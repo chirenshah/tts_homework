adbedff20b2bdf6f29344d24ec52455ae61cbfa9
Chiren Shah <shah.chiren25@gmail.com>
2022-08-15 02:19:17 -0700

    added java_intentional Mistakes Homework

---
+++ Homework/helloworld/src/helloworld/testClass.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class testClass {
+	public static void main(String[] args) {
+		String line = 2; // incorrect declaration
+		Integer sequence;
+		System.out.print(sequence); // printing value without intialization
+		if(true) {
+			String line = "abc";
+		}
+		else {
+			System.out.println("");
+		}
+		
+	}
+}
